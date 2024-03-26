f9f8a860807dbc2fdb5cd77a5718ac2c3aca9cd7
dell <sxy759334746@126.com>
2024-03-26 16:20:29 +0800

    3月26号commit 更新到图算法

--- src/Tree.java
+++ src/Tree.java
@@ -204,7 +204,45 @@ public class Tree {
     //记录变量
     //int pre = Integer.MIN_VALUE; 每次打印时候比较 如果有不是的就false
 
+    //判断是不是二叉搜索树 最主要的是用中序遍历  把打印动作替换为比较pre和当前
+    //或者用list存起来再判断
+    //对于递归还是非递归其实是一样的 都可以写list存 或者直接比较
 
+    //checkBST
+    public static int perValueForCheckBST = Integer.MIN_VALUE;
+    public static boolean checkBST(TreeNode head){
+        if(head == null) return true;
+
+        boolean isLeftBst = checkBST(head.left);
+        if (!isLeftBst) return false;
+        if (head.value <= perValueForCheckBST){
+            return false;
+        }else {
+            perValueForCheckBST = head.value;
+        }
+
+        return checkBST(head.right);
+    }
+
+    //这种是使用递归的 list方法
+    public static boolean checkBST2(TreeNode head){
+        List<TreeNode> inOrderList = new ArrayList<>();
+        processForBST(head,inOrderList);
+
+        for (int i = 1; i < inOrderList.size(); i++) {
+
+            if (inOrderList.get(i).value <= inOrderList.get(i-1).value){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void processForBST(TreeNode head,List<TreeNode> inOrderList){
+        if (head == null) return;
+        processForBST(head.left,inOrderList);
+        inOrderList.add(head);
+        processForBST(head.right,inOrderList);
+    }
 
     //判断是不是完全二叉树
     //完全二叉树 进行宽度遍历 任意节点 有右边无左边 直接false
@@ -240,10 +278,8 @@ public class Tree {
         return true;
     }
 
-    //判断是不是 满二叉树 平衡二叉树
-    //满二叉树就是 最大深度
-
-    //求一颗二叉树的最大深度  满二叉树的节点数量是（2的 最大深度次方）- 1
+    //判断是不是 满二叉树
+    //求出一颗二叉树的最大深度  满二叉树的节点数量是（2的 最大深度次方）- 1
     public static int maxDepth(TreeNode head) {
         if (head == null) return 0;
 //        return Math.max(maxDepth(head.left), maxDepth(head.right)) + 1;
@@ -251,10 +287,12 @@ public class Tree {
         int right = maxDepth(head.right);
         return  Math.max(left, right) + 1;
     }
-
     //代码暂时不全
 
-    //平衡二叉树 任何一个子树 左子树和右子树高度差不超过1
+
+
+    //平衡二叉树 任何一个子树 左平衡 右平衡  左子树和右子树高度差不超过1
+
     public static boolean isBalance(TreeNode head) {
 
         if (head == null) return true;
@@ -264,17 +302,238 @@ public class Tree {
     }
 
 
+    //从结构体开始学递归
+    public static class ReturnTypeForBalanceTree {
+        public boolean isBalanced;
+        public int height;
+        public ReturnTypeForBalanceTree(boolean isBalanced, int height) {
+            this.isBalanced = isBalanced;
+            this.height = height;
+        }
+    }
+
+    //然后写basecase就是最基础的return的时候的例子
+    public static ReturnTypeForBalanceTree processForBalance(TreeNode x) {
+        if (x == null) return new ReturnTypeForBalanceTree(true, 0);
+        ReturnTypeForBalanceTree leftData = processForBalance(x.left);
+        ReturnTypeForBalanceTree rightData = processForBalance(x.right);
+
+        int height = Math.max(leftData.height, rightData.height) + 1;
+        boolean isBalanced = leftData.isBalanced && rightData.isBalanced && Math.abs(leftData.height - rightData.height) <= 1;
+
+        return  new ReturnTypeForBalanceTree(isBalanced, height);
+
+    }
+
+    //然后是递归函数
+    public static boolean isBalance2(TreeNode head) {
+        return processForBalance(head).isBalanced;
+    }
+
+    //同样的开始写二叉搜索树的递归判断
+    //左搜索 右搜索 然后左的max 小于x  右的min 大于x
+    //还是 信息体 basecase 然后递归
+
+    //信息体
+    public static class ReturnDataForBST {
+        public int min;
+        public int max;
+        public boolean isBST;
+        public ReturnDataForBST(int min,int max, boolean isBST){
+            this.isBST = isBST;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    public static ReturnDataForBST processBST(TreeNode x){
+        if(x == null) return null;
+
+        ReturnDataForBST leftData = processBST(x.left);
+
+        ReturnDataForBST rightData = processBST(x.right);
+
+        int min = x.value;
+        int max = x.value;
+
+        if(leftData != null){
+            min = Math.min(min, leftData.min);
+            max = Math.max(max, leftData.max);
+        }
+
+        if(rightData != null){
+            min = Math.min(min, rightData.min);
+            max = Math.max(max, rightData.max);
+        }
+
+        boolean isBST = true;
+        if(leftData != null && !leftData.isBST) isBST = false;
+        if(rightData != null && !rightData.isBST) isBST = false;
+        if(leftData != null && x.value <= leftData.max) isBST = false;
+        if(rightData != null && x.value >= rightData.min) isBST = false;
+
+        return new ReturnDataForBST(min,max,isBST);
+
+    }
+
+    //递归判断是不是满二叉树
+    public static class ReturnDataForFullTree {
+        public int size;
+        public int height;
+        public ReturnDataForFullTree(int size, int height) {
+            this.size = size;
+            this.height = height;
+        }
+    }
+    public static ReturnDataForFullTree processFullTree(TreeNode x){
+        if (x== null) return new ReturnDataForFullTree(0,0);
+        ReturnDataForFullTree isFullLeft = processFullTree(x.left);
+        ReturnDataForFullTree isFullRight = processFullTree(x.right);
+
+        int height = Math.max(isFullLeft.height, isFullRight.height) + 1;
+        int size = isFullLeft.size + isFullRight.size + 1;
+
+        return new ReturnDataForFullTree(size, height);
+    }
+    public static boolean isFullTree(TreeNode x){
+        if (x== null) return true;
+
+        ReturnDataForFullTree data = processFullTree(x);
+
+//        return data.size == (1<< data.height - 1);
+        if (data.size != (Math.pow(2, data.height) - 1))return false;
+
+        return true;
+
+    }
+
+    //返回两个二叉树的最低公共祖先 两种解法 麻烦的 需要用hashmap记录节点和父节点
+    //并递归记录所有节点和其父节点
+    //然后从节点a开始 把所有祖先节点放入hashset 然后b的祖先与set里的节点比较
+    //遇到的第一个就是最小公共祖先
+    public static TreeNode lCA(TreeNode root, TreeNode A, TreeNode B) {
+        HashMap<TreeNode,TreeNode> fatherMap = new HashMap<>();
+        fatherMap.put(root,root);
+
+        findFather(root,fatherMap);
+        HashSet<TreeNode> set = new HashSet<>();
+        TreeNode cur = A;
+        while (cur != fatherMap.get(cur)){
+            set.add(cur);
+            cur = fatherMap.get(cur);
+        }
+
+        cur = B;
+        while (!set.contains(cur)){
+            cur = fatherMap.get(cur);
+        }
+        return cur;
+    }
+    public static void findFather(TreeNode root,HashMap<TreeNode,TreeNode> fatherMap){
+        if (root == null)return;
+
+        if (root.left != null)fatherMap.put(root.left,root);
+        if (root.right != null)fatherMap.put(root.right,root);
+        findFather(root.left,fatherMap);
+        findFather(root.right,fatherMap);
+
+    }
+
+    //第二种解法 还是递归
+    public static TreeNode lCA2(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null || root == A || root == B)return root;//basecase
+        TreeNode left = lCA2(root.left,A,B);
+        TreeNode right = lCA2(root.right,A,B);
+        if(left != null && right != null){
+            return root;
+        }
+
+        return left != null ? left : right;
+
+//        //或者这样写 更好理解
+
+//        if (root == null || root == A || root == B)return root;//basecase
+//        TreeNode left = lCA2(root.left,A,B);
+//        TreeNode right = lCA2(root.right,A,B);
+
+//        if(left != null && right != null)return root;
+//        if(left != null) return left;
+//        if(right != null) return right;
+
+    }
+
+    // 在二叉树中找到后继节点 后继节点就是中序遍历中的下一个节点 左子树的最右节点
+    //如果常规情况直接中序遍历 但是要优化
+    //分两种情况 1我有右子树，右子树的最左节点就是我的后继节点
+    //2 x没有右树 我是不是我父节点的左节点
+    //没有就返回父节点 直到找到父节点的左节点y就是x的后继
+    //但是考虑二叉树上总有一个节点没有父节点 所以他没右子树 也没有祖先结点是左节点
+
+    //这个需要TreeNode类里有个指针指向父节点
+    //因为treenode类没有 parent指针 所以注释了
+
+//    public static TreeNode findSuccessor(TreeNode node){
+//        if (node == null) return null;
+//        if (node.right != null){
+//            return findLeftMost(node.right);
+//        }else { //没有右子树
+//            TreeNode parent = node.parent;
+//            while (parent != null && node != parent.left){
+//                //找到一个节点是我父亲的左孩子 这个节点的父节点就是后继节点
+//                node = parent;
+//                parent = node.parent;
+//            }
+//            return parent;
+//        }
+//
+//    }
+//
+//  //node == node.parent.left ? return node.parent : node = node.parent;
+
+    public static TreeNode findLeftMost(TreeNode node){
+        if (node == null) return null;
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+
+    //二叉树的序列化和反序列化
 
 
+    //折痕凹凸问题 中序遍历
+    public static void printAllFolds( int N) {
+        printProcess(1,N,true);
 
+    }
 
+    //true 就是凹痕 false就是凸痕
+    public static void printProcess(int i, int N, boolean down) {
+        if (i > N)  return;
+        printProcess(i+1,N,true);
+        System.out.println(down ? "ao" : "tu");
+        printProcess(i+1,N,false);
 
-
-
-
-
+    }
 
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
